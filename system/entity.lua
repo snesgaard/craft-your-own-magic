6fdcdd96dfe7f0fff95443e53db8a9f9b58e312b
Sebastian Jensen <SJensen4@its.jnj.com>
2022-12-11 12:55:01 +0100

    added entity system test

--- system/entity.lua
+++ system/entity.lua
@@ -4,13 +4,19 @@ local entity = Base()
 
 function entity:destroy(entity) entity:destroy() end
 
-function entity:spawn_from(parent, ...)
+local function noop() end
+
+function entity:spawn_from(parent, func, ...)
     local team = parent:get(nw.component.team)
 
-    return entity:world():entity()
+    return parent:world():entity()
         :set(nw.component.parent, parent.id)
         :set(nw.component.team, team)
-        :assemble(...)
+        :assemble(func or noop, ...)
+end
+
+function entity:spawn(parent)
+    return parent:world():entity()
 end
 
 return entity.from_ctx
