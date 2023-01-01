dcf8069c11c6d009c009319524f6e078d0d89ac3
Sebastian Jensen <s.nesgaard@gmail.com>
2023-01-01 14:40:43 +0100

    patrol#

--- ai/action/init.lua
+++ ai/action/init.lua
@@ -23,4 +23,23 @@ function rh.wait(ctx, duration)
     ctx:spin(function() return is_done:peek() end)
 end
 
+local function patrol_initial_position(entity, patrol_positions)
+    local pos = entity:get(nw.component.position) or vec2()
+    return patrol_positions
+        :map(function(p) return (p - pos):length() end)
+        :argsort()
+        :head()
+end
+
+function rh.patrol(ctx, entity, patrol_positions, speed, wait_time)
+    if #patrol_positions == 0 then return end
+
+    local init = patrol_initial_position(entity, patrol_positions)
+    for i = init, #patrol_positions do
+        local pos = patrol_positions[i]
+        rh.move(ctx, entity, pos, speed)
+        rh.wait(ctx, wait_time)
+    end
+end
+
 return setmetatable(rh, rh)
