fce97f748cb33c0b70d438d024241b19b22b453f
Sebastian Jensen <s.nesgaard@gmail.com>
2023-05-12 07:08:27 +0200

    some painter font drawing stuff

--- main.lua
+++ main.lua
@@ -1,80 +1,14 @@
 nw = require "nodeworks"
-input = require "system.input"
-mouse = require "system.mouse"
-button = require "system.button"
-slider = require "system.slider"
 painter = require "painter"
 
 decorate(nw.component, require "component", true)
 
 Frame.slice_to_pos = Spatial.centerbottom
 
-
 function love.load(args)
     if args[1] == "test" then
         require "test"
         return love.event.quit()
     end
-
-    ecs_world = nw.ecs.entity.create()
-
-    ecs_world:entity("button")
-        :set(nw.component.hitbox, 100, 100, 100, 100)
-        :set(nw.component.mouse_rect, 100, 100, 100, 100)
-        :set(nw.component.drawable, nw.drawable.body)
-
-    ecs_world:entity("slider")
-        :set(nw.component.mouse_rect, 100, 300, 100, 20)
-        :set(nw.component.drawable, slider.draw_widget)
-        :set(nw.component.slider, 50, 0, 100)
 end
 
-local function color_is_down(entity)
-    if entity:get(nw.component.is_down) then
-        entity:set(nw.component.color, 1, 0, 0)
-    else
-        entity:remove(nw.component.color)
-    end
-end
-
-local function handle_gui_logic(ecs_world)
-    if ecs_world:get(nw.component.pressed, "button") then
-        print("sonic boom!")
-    end
-
-    color_is_down(ecs_world:entity("button"))
-    color_is_down(ecs_world:entity("slider"))
-end
-
-function love.update(dt)
-    nw.system.entity():emit(ecs_world, nw.component.update, dt)
-
-    while nw.system.entity():spin(ecs_world) > 0 do
-        mouse.spin(ecs_world)
-        button.spin(ecs_world)
-        slider.spin(ecs_world)
-    end
-    
-    handle_gui_logic(ecs_world)
-end
-
-function love.draw()
-    painter.draw(ecs_world)
-end
-
-function love.keypressed(key, scancode, isrepeat)
-    if key == "escape" then love.event.quit() end
-    input.keypressed(ecs_world, key)
-end
-
-function love.mousepressed(x, y, button, isTouch)
-    input.mousepressed(ecs_world, x, y, button, isTouch)
-end
-
-function love.mousereleased(x, y, button, isTouch)
-    input.mousereleased(ecs_world, x, y, button, isTouch)
-end
-
-function love.mousemoved(x, y, dx, dy)
-    input.mousemoved(ecs_world, x, y, dx, dy)
-end
--- painter.lua
+++ painter.lua
@@ -1,46 +1,82 @@
 local painter = {}
 
+painter.scale = 4
+
 local layers = {
     background = -1,
     player = 1,
     effects = 2
 }
 
-local function sort_by_position(a, b)
-    local pos_a = a:ensure(nw.component.position)
-    local pos_b = b:ensure(nw.component.position)
+painter.cache_font = {}
 
-    local dx = pos_a.x - pos_b.x
-
-    if math.abs(dx) > 1 then return pos_a.x < pos_b.x end
-
-    return pos_a.y < pos_b.y
+function painter.font(size)
+    local path = "art/font/m5x7.ttf"
+    painter.cache_font[size] = painter.cache_font[size] or gfx.newFont(path, size, "mono")
+    return painter.cache_font[size]
 end
 
-local function sort_by_layer(a, b)
-    local layer_a = a:ensure(nw.component.layer)
-    local layer_b = b:ensure(nw.component.layer)
+function painter.screen_size()
+    return gfx.getWidth() / painter.scale, gfx.getHeight() / painter.scale
+end
+
+function painter.relative(rx, ry)
+    local w, h = painter.screen_size()
+    return w * rx, h * ry
+end
+
+local function remove_hidden(id)
+    return not stack.get(nw.component.hidden, id)
+end
+
+local function sort_drawers(a, b)
+    local layer_a = stack.get(nw.component.layer, a) or 0
+    local layer_b = stack.get(nw.component.layer, b) or 0
 
     if layer_a ~= layer_b then return layer_a < layer_b end
 
-    return sort_by_position(a, b)
+    local pos_a = stack.ensure(nw.component.position, a)
+    local pos_b = stack.ensure(nw.component.position, b)
+
+    return pos_a.x < pos_b.x
 end
 
-local function get_entity(id, ecs_world) return ecs_world:entity(id) end
-
-function painter.draw(ecs_world)
-    local drawables = ecs_world:get_component_table(nw.component.drawable)
-    local entities = drawables
-        :keys()
-        :map(get_entity, ecs_world)
-        :sort(sort_by_layer)
-
-    for _, entity in ipairs(entities) do
-        local f = entity:get(nw.component.drawable)
-        gfx.push("all")
-        f(entity)
-        gfx.pop()
+local function draw_entity(ids)
+    for _, id in ipairs(ids) do
+        local drawable = stack.get(nw.component.drawable, id)
+        if drawable then drawable(id) end
     end
+end
+
+function painter.draw()
+    gfx.push()
+    gfx.scale(painter.scale, painter.scale)
+    stack.get_table(nw.component.drawable):keys()
+        :filter(remove_hidden)
+        :sort(sort_drawers)
+        :visit(draw_entity)
+    gfx.pop()
+end
+
+local DEFAULT_OPT = {
+    align = "left",
+    valign = "top",
+    scale = painter.scale,
+    font = painter.font(8)
+}
+
+function painter.draw_text(text, area, opt)
+    local opt = opt or DEFAULT_OPT
+    local align = opt.align or DEFAULT_OPT.align
+    local valign = opt.valign or DEFAULT_OPT.valign
+    local s = opt.scale or DEFAULT_OPT.scale
+    local font = opt.font or DEFAULT_OPT.font
+    local dy = compute_valign(text, font, area.w, area.h, 1.0 / s, valign)
+    gfx.printf(
+        text, font, area.x, area.y + dy,
+        area.w * s, align,
+        0, 1.0 / s, 1.0 / s
+    )
 end
 
 return painter
