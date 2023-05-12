181e7159517bb6ffa37111fecf6275f7c8660dd9
Sebastian Jensen <s.nesgaard@gmail.com>
2023-05-12 07:20:18 +0200

    initial setup

--- main.lua
+++ main.lua
@@ -1,14 +1,46 @@
 nw = require "nodeworks"
 painter = require "painter"
+stack = nw.ecs.stack
+
+-- System shortcuts
+event = nw.system.event
+input = nw.system.input
+collision = nw.system.collision
 
 decorate(nw.component, require "component", true)
 
 Frame.slice_to_pos = Spatial.centerbottom
+
+local function spin()
+    while event.spin() > 0 do
+
+    end
+end
 
 function love.load(args)
     if args[1] == "test" then
         require "test"
         return love.event.quit()
     end
+
+    collision.register("test", spatial(10, 10, 100, 100))
 end
 
+function love.update(dt)
+    event.emit("update", dt)
+    spin()
+end
+
+function love.draw()
+    painter.draw()
+    collision.draw()
+end
+
+function love.keypressed(key)
+    if key == "escape" then love.event.quit() end
+    input.keypressed(key)
+end
+
+function love.keyreleased(key)
+    input.keyreleased(key)
+end
